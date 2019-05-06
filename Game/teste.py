b58fef6b0233790533951698534b358389dbcaff
RafaelHBZ <rafaelrhbz@gmail.com>
2019-05-06 19:31:55 -0300

    Modificação 1

---
+++ Game/teste.py
@@ -0,0 +1,19 @@
+cenarios = {
+        "inicio": {
+            "titulo": "Saguao do perigo",
+            "descricao": "Voce esta no saguao de entrada do insper",
+            "opcoes": {
+                "andar professor": "Tomar o elevador para o andar do professor",
+                "biblioteca": "Ir para a biblioteca",
+                }
+            }
+        }
+
+
+#print(cenarios.values())
+
+for i in cenarios["inicio"]:
+    print (i)
+    
+    
+    
